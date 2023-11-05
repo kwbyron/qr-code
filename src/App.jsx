fa497210a89b6edd6223658886c95f8324485c1a
Keoni <keoni@Keonis-MacBook-Pro.local>
2023-11-04 21:17:51 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='qr-box'>
+      <img className='qr-img' src='./image-qr-code.png' alt='qrcode'/>
+      <h3>Improve your front-end skills by building projects</h3>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+    </>
+  )
+}
+
+export default App
